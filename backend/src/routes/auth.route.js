2d7fc6f05e983a406f1f83be8a96ce7bd34faf16
cloudwizardx <cloudwizardx@gmail.com>
2025-07-17 13:48:36 +0700

    feat: connectDb config MongoDB and UserModels

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,16 +1,12 @@
 import express from 'express'
+import { login, logout, signup } from '../controllers/auth.controller.js'
 
 const router = express.Router()
 
-router.get('/signup', (req, res) => {
+router.get('/signup', signup)
 
-})
+router.get('/login', login)
 
-router.get('/login', (req, res) => {
+router.get('/logout', logout)
 
-})
-
-router.get('/logout', (req, res) => {
-
-})
-
+export default router
