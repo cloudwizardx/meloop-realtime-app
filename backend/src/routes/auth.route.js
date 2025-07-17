aa582c71a1dca08d46d6476dd8ffcb1c08e0073d
cloudwizardx <cloudwizardx@gmail.com>
2025-07-17 12:17:22 +0700

    first commit

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.get('/signup', (req, res) => {
+
+})
+
+router.get('/login', (req, res) => {
+
+})
+
+router.get('/logout', (req, res) => {
+
+})
+
---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import authRoutes from './routes/auth.route.js'
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT
+
+app.use('api/auth', authRoutes)
+
+app.listen(port, () => {
+    console.log('Server is running on port 5001')
+})
