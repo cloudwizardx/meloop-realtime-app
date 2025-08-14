9829b9644bbf491dcf732fa787a663f5886d8674
cloudwizardx <cloudwizardx@gmail.com>
2025-08-14 19:55:13 +0700

    chore(be): saving cached key base on role and level remove on userId

--- backend/src/services/permission.service.ts
+++ backend/src/services/permission.service.ts
@@ -1,25 +1,25 @@
-import { Types } from 'mongoose'
 import redisClient from '~/configs/redis.config'
 import UserPermissionModel from '~/models/database/permission.model'
 
 interface PermissionForRoleParams {
-  userId: Types.ObjectId
   role: string
+  level: string
 }
 
-export const getPermissionForRole = async ({ userId, role }: PermissionForRoleParams) => {
-  const cacheKey = `role_permission:${role}`
+export const getPermissionForRole = async ({ role, level }: PermissionForRoleParams) => {
+  const cacheKey = `role_permission:${role.toLowerCase()}:${level.toLowerCase()}`
 
   const cachedPermission = await redisClient.get(cacheKey)
   if (cachedPermission) {
     return JSON.parse(cachedPermission)
   }
 
-  const permissionDoc = await UserPermissionModel.findOne({ userId: userId, role: role })
+  const permissionDoc = await UserPermissionModel.findOne({ level: level, role: role })
   const permissions = permissionDoc?.permissions || []
 
+  const ttl = parseInt(process.env.REDIS_TTL!)
   await redisClient.set(cacheKey, JSON.stringify(permissions), {
-    EX: 600
+    EX: ttl
   })
 
   return permissions
