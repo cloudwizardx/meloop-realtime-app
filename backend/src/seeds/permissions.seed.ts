c37a8bf708e484dc9a57db76debb7527e0021926
cloudwizardx <cloudwizardx@gmail.com>
2025-08-14 19:49:39 +0700

    feat(be): seed permissions for role and level member

---
+++ backend/src/seeds/permissions.seed.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+import UserPermissionModel from '../models/database/permission.model'
+import { USER_PERMISSIONS } from '../utils/app.constant'
+
+async function connectDB() {
+  await mongoose.connect(process.env.MONGO_URI!)
+}
+
+async function seedRolePermissions() {
+  const permissionRoleUserLevelNormal = {
+    role: 'User',
+    level: 'Normal',
+    permissions: Object.values(USER_PERMISSIONS)
+  }
+
+  await UserPermissionModel.updateOne(
+    { role: permissionRoleUserLevelNormal.role, level: permissionRoleUserLevelNormal.level },
+    { $set: { permissions: permissionRoleUserLevelNormal.permissions } },
+    { upsert: true }
+  )
+}
+
+;(async () => {
+  await connectDB()
+  await seedRolePermissions()
+  process.exit(0)
+})()
