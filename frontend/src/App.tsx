49c4d4b9ed2c05de13d8c23ab71a02629bf90392
cloudwizardx <cloudwizardx@gmail.com>
2025-08-20 01:15:36 +0700

    feat(fe): implement MessagePage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { HomePage } from "./pages/HomePage"
 import { LoginPage } from "./pages/LoginPage"
+import { MessagePage } from "./pages/MessagePage"
 import { ProfilePage } from "./pages/ProfilePage"
 
 
 function App() {
-  return (<ProfilePage></ProfilePage>)
+  return (<MessagePage></MessagePage>)
 }
 
 export default App
